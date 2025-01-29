276d18d12371f3a26d142d79fc59a7cabe6524ca
jackfromnewyork <10056954@network.rca.ac.uk>
2025-01-29 16:47:36 +0000

    create first template

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(640, 640);
+}
+
+function draw() {
+  background(220, 90, 125);
+  stroke(110, 0, 32)
+strokeWeight(3)
+  fill(255, 192, 203)
+  circle(150, 60, 50)
+}
